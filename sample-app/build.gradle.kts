98479c1992fa6a64668e06663323fc40fbe38c97
Derek Eskens <snekse@gmail.com>
2026-01-04 20:53:36 -0600

    Add dependencies to sample app

--- sample-app/build.gradle.kts
+++ sample-app/build.gradle.kts
@@ -1,11 +1,20 @@
 plugins {
     id("org.springframework.boot")
+    id("groovy")
 }
 
 dependencies {
     implementation(project(":resequence-starter"))
+    implementation("org.springframework.kafka:spring-kafka")
+    implementation("com.fasterxml.jackson.core:jackson-databind")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.kafka:spring-kafka-test")
+    testImplementation("org.spockframework:spock-core:2.4-groovy-5.0")
+    testImplementation("org.spockframework:spock-spring:2.4-groovy-5.0")
 }
 
 tasks.withType<Test> {
